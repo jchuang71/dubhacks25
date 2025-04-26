e8de32fb949e2bbea16df4ab155e808750251dbb
ilya <ilyavas@uw.edu>
2025-04-26 13:51:31 -0700

    player mend forest not done

--- Seedfall/Assets/Scripts/Player.cs
+++ Seedfall/Assets/Scripts/Player.cs
@@ -1,37 +1,63 @@
 using UnityEngine;
+using Photon.Pun;
+using ExitGames.Client.Photon.StructWrapping; // Import the Photon Unity Networking namespace
 
-public class Player : MonoBehaviour
+public class Player : MonoBehaviourPun
 {
     float speed = 5f; // Speed of the player movement
+    public VegetationArea vegetationArea; // Reference to the VegetationArea script
+    public GameObject tileStandingOn; // Reference to the tile the player is standing on
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        PlayerMovement(); // Call the PlayerMovement function every frame
+        //tileStandingOn = null;
+        if(photonView.IsMine) // Check if this player instance is controlled by the local player
+        {
+            PlayerMovement(); // Call the PlayerMovement function every frame
+            PlayerMendForest(); // Call the PlayerMendForest function every frame
+        }
     }
 
     void PlayerMovement()
     {
         if(Input.GetKey(KeyCode.W))
         {
-            transform.Translate(Vector3.forward * Time.deltaTime * speed); // Move forward
+            transform.Translate(new Vector2(0,1) * Time.deltaTime * speed); // Move up
         }
         else if(Input.GetKey(KeyCode.A))
         {
-            transform.Translate(Vector3.left * Time.deltaTime * speed); // Move left
+            transform.Translate(new Vector2(-1,0) * Time.deltaTime * speed); // Move left
         }
         else if(Input.GetKey(KeyCode.S))
         {
-            transform.Translate(Vector3.back * Time.deltaTime * speed); // Move backward
+            transform.Translate(new Vector2(0,-1) * Time.deltaTime * speed); // Move down
         }
         else if(Input.GetKey(KeyCode.D))
         {
-            transform.Translate(Vector3.right * Time.deltaTime * speed); // Move right
+            transform.Translate(new Vector2(1,0) * Time.deltaTime * speed); // Move right
         }
+    }
+
+    void PlayerMendForest()
+    {
+        if(Input.GetKey(KeyCode.E))
+        {
+            if(tileStandingOn != null)
+            {
+                tileStandingOn.GetComponent<VegetationTile>().ChangeState("High"); // Change the state of the tile to "Low"
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        tileStandingOn = collision.gameObject;
     }
 }
