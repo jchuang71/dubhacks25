79550e133d8d71abb9642d8d226d3494c2b51703
ManYak404 <ilya.vaschillo@gmail.com>
2025-04-25 18:41:55 -0700

    initial project + game manager object

---
+++ Seedfall/Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager ManagerInstance; // Singleton reference to this manager object available to all other scripts
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
