8b225af27c91869c2a41e5531890797951e88955
UriMittelmanProjects <urimittelman05@gmail.com>
2025-04-26 23:24:15 -0700

    Very basic room functionality added

---
+++ Seedfall/Assets/Scripts/LobbyManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    public TMP_InputField roomInputField;
+    public GameObject lobbyPanel;
+    public GameObject roomPanel;
+    public TMP_Text roomName;
+
+    private void Start()
+    {
+        PhotonNetwork.JoinLobby();
+    }
+
+    public void OnClickCreate()
+    {
+        PhotonNetwork.CreateRoom(roomInputField.text, new RoomOptions() { MaxPlayers = 2 });
+    }
+
+    public override void OnJoinedRoom()
+    {
+        lobbyPanel.SetActive(false);
+        roomPanel.SetActive(true);
+        roomName.text = "Room Name: " + PhotonNetwork.CurrentRoom.Name;
+    }
+}
