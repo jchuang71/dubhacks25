6972d7ddda010e6c64f768b2917d74f4569634f5
UriMittelmanProjects <urimittelman05@gmail.com>
2025-04-27 00:16:59 -0700

    Bugfixed some stuff still need to add refresh button for room list and other stuff

--- Seedfall/Assets/Scripts/LobbyManager.cs
+++ Seedfall/Assets/Scripts/LobbyManager.cs
@@ -4,6 +4,9 @@ using Photon.Realtime;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.SceneManagement;
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine.UIElements;
 
 public class LobbyManager : MonoBehaviourPunCallbacks
 {
@@ -11,6 +14,15 @@ public class LobbyManager : MonoBehaviourPunCallbacks
     public GameObject lobbyPanel;
     public GameObject roomPanel;
     public TMP_Text roomName;
+    public GameObject roomsList;
+    public GameObject leaveButton;
+
+    public RoomItem roomItemPrefab;
+    List<RoomItem> roomItemsList = new List<RoomItem>();
+    public Transform contentObject;
+
+    public float timeBetweenUpdates = 1.5f;
+    float nextUpdateTime;
 
     private void Start()
     {
@@ -25,7 +37,58 @@ public class LobbyManager : MonoBehaviourPunCallbacks
     public override void OnJoinedRoom()
     {
         lobbyPanel.SetActive(false);
+        roomsList.SetActive(false);
         roomPanel.SetActive(true);
+        leaveButton.SetActive(true);
         roomName.text = "Room Name: " + PhotonNetwork.CurrentRoom.Name;
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+
+        if(Time.time >= nextUpdateTime)
+        {
+            UpdateRoomList(roomList);
+            nextUpdateTime = Time.time + timeBetweenUpdates;
+        }
+    }
+
+    private void UpdateRoomList(List<RoomInfo> list)
+    {
+        foreach (RoomItem item in roomItemsList)
+        {
+            Destroy(item.gameObject);
+        }
+        roomItemsList.Clear();
+
+        foreach (RoomInfo room in list)
+        {
+            RoomItem newRoom = Instantiate(roomItemPrefab, contentObject);
+            newRoom.SetRoomName(room.Name);
+            roomItemsList.Add(newRoom);
+        }
+    }
+
+    public void JoinRoom(string roomName)
+    {
+        PhotonNetwork.JoinRoom(roomName);
+    }
+
+    public void OnClickLeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+
+    public override void OnLeftRoom()
+    {
+        roomPanel.SetActive(false);
+        leaveButton.SetActive(false);
+        lobbyPanel.SetActive(true);
+        roomsList.SetActive(true);
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
     }
 }
